d42ca2a92c6529fb0675cafa34f919e4ef311da9
s.v.zaytsev <s.v.zaytsev@tinkoff.ru>
2024-05-24 10:21:18 +0500

    feat(prettier-config)!: upgrade prettier to v3

--- packages/prettier-config/index.js
+++ packages/prettier-config/index.js
@@ -1,9 +1,7 @@
-const config = {
+export default {
   tabWidth: 2,
   singleQuote: true,
   trailingComma: 'es5',
   arrowParens: 'always',
   htmlWhitespaceSensitivity: 'strict',
 };
-
-module.exports = config;
--- packages/prettier-config/plugins/prettier-plugin-sort-package.js
+++ packages/prettier-config/plugins/prettier-plugin-sort-package.js
@@ -1,38 +1,42 @@
-const sortPackageJson = require('sort-package-json');
-const { parsers } = require('prettier/parser-babel');
+import sortPackageJson from 'sort-package-json';
+import parserBabel from 'prettier/parser-babel';
 
-const parser = parsers['json-stringify'];
+const {
+  parsers: { 'json-stringify': parser },
+} = parserBabel;
 
-exports.parsers = {
-  'json-stringify': {
-    ...parser,
-    preprocess(text, options) {
-      const processed = parser.preprocess
-        ? parser.preprocess(text, options)
-        : text;
+export default {
+  parsers: {
+    'json-stringify': {
+      ...parser,
+      preprocess(text, options) {
+        const processed = parser.preprocess
+          ? parser.preprocess(text, options)
+          : text;
 
-      const isPackageJson =
-        options.filepath &&
-        /package\.json$|ng-package\.json$/.test(options.filepath);
+        const isPackageJson =
+          options.filepath &&
+          /package\.json$|ng-package\.json$/.test(options.filepath);
 
-      if (isPackageJson) {
-        const json = JSON.parse(processed);
-        const unsortedScripts = deepClone((json && json.scripts) || {});
-        const sorted = sortPackageJson(json);
+        if (isPackageJson) {
+          const json = JSON.parse(processed);
+          const unsortedScripts = deepClone((json && json.scripts) || {});
+          const sorted = sortPackageJson(json);
 
-        /**
-         * @note: add the scripts field if it's provided
-         * the scripts must be unsorted
-         */
-        // eslint-disable-next-line no-prototype-builtins
-        if (json && json.hasOwnProperty('scripts')) {
-          sorted.scripts = unsortedScripts;
+          /**
+           * @note: add the scripts field if it's provided
+           * the scripts must be unsorted
+           */
+          // eslint-disable-next-line no-prototype-builtins
+          if (json && json.hasOwnProperty('scripts')) {
+            sorted.scripts = unsortedScripts;
+          }
+
+          return JSON.stringify(sorted);
         }
 
-        return JSON.stringify(sorted);
-      }
-
-      return processed;
+        return processed;
+      },
     },
   },
 };
